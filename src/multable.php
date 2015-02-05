c1fcaa5f7fe5713cf716ae5212d9a5f51174652b
Harrings <sean.harrington2@gmail.com>
2015-02-04 18:47:33 -0800

    Finished Mult table

--- src/multable.php
+++ src/multable.php
@@ -9,74 +9,75 @@ ini_set('display_errors','On');
 </head>
 <body>
 <?php
-var $errors=0;
+ $errors=0;
 //checking inputs exist
 if ($_GET['min-multiplicand']==null)
 {
 	echo "Minimum multiplicand not given <br>";
-	errors++;
+	$errors++;
 }
 if ($_GET['max-multiplicand']==null)
 {
 	echo "Max multiplicand not given <br>";
-	errors++;
+	$errors++;
 }
 if ($_GET['min-multiplier']==null)
 {
 	echo "Minimum multiplier not given <br>";
-	errors++;
+	$errors++;
 }
 if ($_GET['max-multiplier']==null)
 {
 	echo "Maximum multiplier not given <br>";
-	errors++;
+	$errors++;
 }
+ $min1=$_GET['min-multiplicand'];
+ $min2=$_GET["min-multiplier"];
+ $max1=$_GET['max-multiplicand'];
+ $max2=$_GET['max-multiplier'];
 //checking if type integer
-if (!is_int($_GET['min-multiplicand']))
+if ($min1!=(integer)$min1)
 {
-	echo "Minimum multiplicand not int. <br>"
-	errors++;
+	echo "Minimum multiplicand not int. <br>";
+	$errors++;
 }
-if (!is_int($_GET['max-multiplicand']))
+if ($max1!=(integer)$max1)
 {
-	echo "Max multiplicand not int. <br>"
-	errors++;
+	echo "Max multiplicand not int. <br>";
+	$errors++;
 }
-if (!is_int($_GET['min-multiplier']))
+if ($min2!=(integer)$min2)
 {
-	echo "Minimum multiplier not int. <br>"
-	errors++;
+	echo "Minimum multiplier not int. <br>";
+	$errors++;
 }
-if (!is_int($_GET['max-multiplier']))
+if ($max2!=(integer)$max2)
 {
-	echo "Maximum multiplier not int. <br>"
-	errors++;
+	echo "Maximum multiplier not int. <br>";
+	$errors++;
 }
 
 	
-var $min1=$_GET['min-multiplicand'];
-var $min2=$_GET['min-multiplier'];
-var $max1=$_GET['max-multiplicand'];
-var $max2=$_GET['max-multiplier'];
-var $i;
-var $j;
-var $product;
+
+ $i;
+ $j;
+ $product;
 
 
 if ($min1>$max1)
 {
 	echo "Minimum multiplicand larger then Maximum <br>";
-	errors++;
+	$errors++;
 }
 if ($min2>$max2)
 {
 	echo "Minimum multiplier larger then Maximum <br>";
-	errors++;
+	$errors++;
 }
 //if no errors displays table
-if (errors==0)
+if ($errors==0)
 {
-	echo "<table style="width:100%">";
+	echo "<table>";
 	echo "  <caption>PHP table</caption>";
 	echo    "<thead>";
 	echo	  "<tr>";
