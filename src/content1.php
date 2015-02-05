29b862d7a93d60e3e9e256f10d5604c975d1620e
Harrings <sean.harrington2@gmail.com>
2015-02-04 23:32:38 -0800

    Fixed redirect issues

--- src/content1.php
+++ src/content1.php
@@ -3,20 +3,14 @@ ob_start(); //from stack overflow
 error_reporting(E_ALL);
 ini_set('display_errors','On');
 session_start();
-if (isset($_POST["username"])&&isset($_SESSION["herebefore"]))
-{
-    header("Location: login.php", true);
-}
 if (!isset($_SESSION["username"]))
 {
 $_SESSION["username"]= $_POST["username"];
-$_POST["username"]=null;
 }
 if (!isset($_SESSION["username"]))
 {
     header("Location: login.php", true);
 }
-$_SESSION["herebefore"]=1;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -25,23 +19,23 @@ $_SESSION["herebefore"]=1;
 </head>
 <body>
 <?php
-if (!isset($_SESSION["visits"]))
-{
-	$_SESSION["visits"]=1;
-}
-else
-{
-	$_SESSION["visits"]=$_SESSION["visits"]+1;
-}
-if ($_POST["username"]==null||$_POST["username"]=="")
+if (($_SESSION["username"]==null||$_SESSION["username"]==""))
 {
 	echo "A username must be entered click <a href=\"login.php\">here</a> to return to login screen";
 }
 else
 {
-echo "Hello " . $_SESSION["username"];
-echo "<br>You have visited this page   $_SESSION[visits]   times";
-$_POST["username"]=null;
+	if (!isset($_SESSION["visits"]))
+	{
+		$_SESSION["visits"]=1;
+	}
+	else
+	{
+		$_SESSION["visits"]=$_SESSION["visits"]+1;
+	}
+	$_SESSION["herebefore"]=1;
+	echo "Hello " . $_SESSION["username"];
+	echo "<br>You have visited this page   $_SESSION[visits]   times";
 }
 
 
--- src/login.php
+++ src/login.php
@@ -4,10 +4,25 @@
   <title>CS 290-Assignment 4 login</title>
 </head>
 <body>
-
+<?php
+session_start();
+ob_start(); //from stack overflow
+error_reporting(E_ALL);
+ini_set('display_errors','On');
+if (isset($_SESSION["username"]))
+{
+	echo "You are already logged in as $_SESSION[username] <br>";
+	echo "if you would like to view content click <a href=\"content1.php\">here</a>";
+}
+else
+{
+?>
 	<form action="content1.php" method="post">
 		<p>Your name: <input type="text" name="username" /></p>
 		<input type="image" src="sherman.gif" name="submit" />
 	</form>
+<?php
+}
+?>
 </body>
 </html>
